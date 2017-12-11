be60cc49d120fc21a46708a50047041872f8b6d5
Alonzo Moses <alonzo.k.moses@gmail.com>
2017-12-11 12:23:28 -0500

    js-updated: questions added

--- script.js
+++ script.js
@@ -1,1 +1,94 @@
+//display questions here
+const myQuestions = [
+  {
+    question: 'Where on a fish would you find its dorsal fin?',
+    answer: {
+      a: 'Stomach',
+      b: 'Head',
+      c: 'Back',
+      d: 'Tail'
+    },
+    correctAnswer: 'Back - Depending on the species, a fish may have one or two dorsal fins.'
+  },
+  {
+    question: 'Originating in northern China, this dog breed is known for its unique blue-black tongue and thick coat of fur.',
+    answer: {
+      a: 'Pekingese',
+      b: 'Shih Tzu',
+      c: 'Shar Pei',
+      d: 'Chow Chow'
+    },
+    correctAnswer: 'Chow Chow - The Chow Chow is one of the few ancient dog breeds still in existence.'
+  },
+  {
+    question: 'This species of bird is the national animal of the United States of America.',
+    answer: {
+      a: 'Black Vulture',
+      b: 'Bald Eagle',
+      c: 'Praire Falcon',
+      d: 'White-tailed Hawk'
+    },
+    correctAnswer: 'Bald Eagle - The Bald Eagle is found only in North America.'
+  },
+  {
+    question: 'With a top speed of five feet per hour, what is the worlds slowest fish?',
+    answer: {
+      a: 'Dwarf Seahorse',
+      b: 'Dab',
+      c: 'Stickleback',
+      d: 'Angelfish'
+    },
+    correctAnswer: 'Dwarf Seahorse - Endangered, the dwarf seahorse is found in the Bahamas and the United States.'
+  },
+  {
+    question: 'All tigers, expect white tigers, have what color of eyes?',
+    answer: {
+      a: 'Orange',
+      b: 'Yellow',
+      c: 'White',
+      d: 'Black'
+    },
+    correctAnswer: 'Yellow - The white tiger typically'
+  },
+  {
+    question: 'Native to the temperate Northern Hemisphere, which member of the birch family "Betulaceae" produces edible nuts of the same name?',
+    answer: {
+      a: 'Pine',
+      b: 'Hazel',
+      c: 'Hornbeam',
+      d: 'Alder'
+    },
+    correctAnswer: 'Hazel - The Celts believed hazelnuts gave one inspirations and wisdom.'
+  },
+  {
+    question: 'Which small mammal has varieties named "Armored", "Mouse" and "Masked"?',
+    answer: {
+      a: 'Weasel',
+      b: 'Pika',
+      c: 'Squirrel',
+      d: 'Shrew'
+    },
+    correctAnswer: 'Shrew - Although a shew looks somewhat like a long-nosed mouse, it is not a rodent, it is a mole.'
+  },
+  {
+    question: 'Which of the following dogs is best-known for its keen sense of smell and tracking abilities?',
+    answer: {
+      a: 'Chihuahua',
+      b: 'Rottweiler',
+      c: 'Bulldog',
+      d: 'Bloodhound'
+    },
+    correctAnswer: 'Bloodhound - Bloodhounds are well-known for their ability to distinguish human scent over great distances.'
+  },
+  {
+    question: 'Currently at 32 species, this land-adapted fish can spend days walking around out of water?',
+    answer: {
+      a: 'Wels Catfish',
+      b: 'Mudskipper',
+      c: 'Tench',
+      d: 'Haddock'
+    },
+    correctAnswer: 'Mudskipper - Mudskippers use their pelvic and pectoral fins to walk on land.'
+  }
+];
 
